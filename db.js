346b8ff048015e4e835fac0fba3005dda380a8ee
binduanisetti1701-source <binduanisetti1701@gmail.com>
2026-08-15 19:22:09 +0530

    Add MongoDB connection utility in db.js

---
+++ db.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require("mongodb");
+
+const uri = process.env.MONGODB_URI || "mongodb://localhost:27017";
+
+const client = new MongoClient(uri);
+
+async function connectToDatabase() {
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    return client.db("secondchance");
+}
+
+module.exports = {
+    connectToDatabase,
+    client
+};
